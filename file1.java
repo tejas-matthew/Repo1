29f28eec5dba960d39838d236845f7870154cc67
tejas-matthew <62717951+tejas-matthew@users.noreply.github.com>
2020-03-26 23:52:22 +0530

    Create file1.java

---
+++ file1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class first
+{
+public static void main(String args[])
+{
+int x=10; 
+int c=20; 
+int a=b=0; 
+System.out.println("Initial value of x is "+x); 
+a=x;
+b=a;
+x=c;
+System.out.println("Value of x after operations is" +x);
+}
+}
